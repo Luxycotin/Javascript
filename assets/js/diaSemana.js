0ac81248c5185ec2d217bc8b938b0209cc4ca97b
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-05-06 10:42:39 -0300

    Se empezó la primera idea del trabajo diaSemana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,19 @@
+let dia = parseInt(prompt('Escribe un numero para referirte a un dia de la semana, entre el 1 y 7'));
+
+ if( dia = 1){
+    console.log('Lunes')
+}else if( dia = 2){
+    console.log('Martes')
+}else if( dia = 3){
+    console.log('Martes')
+}else if( dia = 4){
+    console.log('Martes')
+}else if( dia = 5){
+    console.log('Martes')
+}else if( dia = 6){
+    console.log('Martes')
+}else if( dia = 7){
+    console.log('Martes')
+}else if( dia != Number){
+    console.log('Lo que ingresaste no corresponde a un dia de la semana')
+}
